cff57faadef806da0dc42352feb005acc82dddb3
ikazerskiy <Ilya.Kazerskiy@artezio.com>
2024-12-01 16:37:17 +0300

    pizza web module

--- pizza-app-api/src/main/java/app/pizza/api/request/CreatePizzaRequest.java
+++ pizza-app-api/src/main/java/app/pizza/api/request/CreatePizzaRequest.java
@@ -3,7 +3,6 @@ package app.pizza.api.request;
 import app.pizza.api.Base;
 import app.pizza.api.Item;
 import java.util.List;
-import java.util.UUID;
 
-public record CreatePizzaRequest(UUID pizzeriaId, Base base, List<Item> items) {
+public record CreatePizzaRequest(Base base, List<Item> items) {
 }
---
+++ pizza-web/src/test/java/app/pizza/web/PizzaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package app.pizza.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
